9d54afc19f2849289b550006f984c8ac28b82bf7
vlad <vladkimfd@gmail.com>
2022-12-23 20:58:28 +0500

    first commit

---
+++ _nuxt/0a2c51f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{299:function(t,n,e){"use strict";e.r(n);var l={},o=e(53),component=Object(o.a)(l,(function(){return(0,this._self._c)("a",{attrs:{href:"../login"}},[this._t("default")],2)}),[],!1,null,null,null);n.default=component.exports}}]);
